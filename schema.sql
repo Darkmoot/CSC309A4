7fdb67fb6fb656310e7f5b2345319562ed842c1b
Darkmoot <kenweilee@gmail.com>
2016-07-25 12:54:14 -0400

    Converted schema to PSQL

--- schema.sql
+++ schema.sql
@@ -1,137 +1,146 @@
 CREATE TABLE Users (
-userid integer primary key autoincrement,
-username text UNIQUE not null CONSTRAINT uname_constraint CHECK (length(username) < 17),
-password text not null CONSTRAINT pw_constraint CHECK (password != '' AND length(password) < 17),
-name text not null CONSTRAINT name_constraint CHECK (length(name) < 101),
-postalcode text not null,
-rating integer default 0
+userid SERIAL PRIMARY KEY,
+username VARCHAR(16) UNIQUE NOT NULL,
+password VARCHAR(16) NOT NULL,
+name VARCHAR(100) NOT NULL,
+postalcode CHAR(7) NOT NULL,
+rating INTEGER default 0
 );
 
 CREATE INDEX user_pcode_index ON Users(postalcode);
 CREATE INDEX user_rating_index ON Users(rating);
 
 CREATE TABLE Parties (
-partyid integer primary key autoincrement,
-partyname text not null CONSTRAINT pname_constraint CHECK (length(partyname) < 101),
-reqskill integer CONSTRAINT reqskill_constraint CHECK (reqskill > 0 AND reqskill < 6),
-reqrating integer CONSTRAINT reqrating_constraint CHECK (reqrating > 0 AND reqrating < 6),
-maxnumpeople integer
+partyid SERIAL PRIMARY KEY,
+partyname VARCHAR(100) NOT NULL,
+reqskill INTEGER CONSTRAINT reqskill_constraint CHECK (reqskill > 0 AND reqskill < 6),
+reqrating INTEGER CONSTRAINT reqrating_constraint CHECK (reqrating > 0 AND reqrating < 6),
+maxnumpeople INTEGER CONSTRAINT maxnumpeople_constraint CHECK (maxnumpeople > 0)
 );
 
 CREATE TABLE Raids (
-raidid integer primary key autoincrement,
-raidname text not null CONSTRAINT rname_constraint CHECK (length(raidname) < 101),
-address text not null CHECK (length(raidname) < 101),
-postalcode text not null,
-raiddate date not null,
-raidtime time not null
+raidid SERIAL PRIMARY KEY,
+raidname VARCHAR(100) NOT NULL,
+address VARCHAR(100) NOT NULL,
+postalcode CHAR(7) NOT NULL,
+raiddate DATE NOT NULL,
+raidtime TIME NOT NULL
 );
 
 CREATE INDEX raid_pcode_index ON Raids(postalcode);
 
 CREATE TABLE Games (
-gameid integer primary key autoincrement,
-gamename text UNIQUE not null CONSTRAINT gname_constraint CHECK (length(gamename) < 101),
-category text,
-minplayers integer not null,
-maxplayers integer not null,
-difficulty integer default 0,
-numdiff integer default 0,
-rating integer default 0,
-numrate integer default 0,
+gameid SERIAL PRIMARY KEY,
+gamename VARCHAR(100) UNIQUE NOT NULL,
+minplayers INTEGER NOT NULL,
+maxplayers INTEGER NOT NULL,
+difficulty INTEGER default 0,
+numdiff INTEGER default 0,
+rating INTEGER default 0,
+numrate INTEGER default 0,
 CONSTRAINT numplayers_constraint CHECK (minplayers <= maxplayers)
 );
 
+CREATE TABLE GameCategory (
+  gamecategory VARCHAR(30) PRIMARY KEY
+);
+
+/* Matches a game to a game category */
+CREATE TABLE OfCategory (
+  gameid INTEGER REFERENCES Games(gameid),
+  gamecategory VARCHAR(30) REFERENCES GameCategory(gamecategory)
+);
+
 CREATE TABLE RaidReviews (
-reviewid integer primary key autoincrement,
-review text
+reviewid SERIAL PRIMARY KEY,
+review VARCHAR(2000)
 );
 
 CREATE TABLE Messages (
-messageid integer primary key autoincrement,
-message text
+messageid SERIAL PRIMARY KEY,
+message VARCHAR(2000)
 );
 
 CREATE TABLE PartyMessages (
-pmessageid integer primary key autoincrement,
-message text
+pmessageid SERIAL PRIMARY KEY,
+message VARCHAR(2000)
 );
 
 /* Matches party message to a sender and a receiving party */
 CREATE TABLE PartyMessaged (
-userid integer references Users(userid)
+userid INTEGER REFERENCES Users(userid)
 ON DELETE CASCADE,
-partyid integer references Parties(partyid)
+partyid INTEGER REFERENCES Parties(partyid)
 ON DELETE CASCADE,
-pmessageid integer references PartyMessages(pmessageid)
+pmessageid INTEGER REFERENCES PartyMessages(pmessageid)
 ON DELETE CASCADE
 );
 
 /* Matches a message to a sender and receiver */
 CREATE TABLE Messaged (
-senderid integer references Users(userid)
+senderid INTEGER REFERENCES Users(userid)
 ON DELETE CASCADE,
-receiverid integer references Users(userid)
+receiverid INTEGER REFERENCES Users(userid)
 ON DELETE CASCADE,
-messageid integer references Messages(messageid)
+messageid INTEGER REFERENCES Messages(messageid)
 ON DELETE CASCADE
 );
 
 /* Matches a review to a reviewer and a raid */
 CREATE TABLE Reviewed (
-revieweeid integer references Users(userid)
+revieweeid INTEGER REFERENCES Users(userid)
 ON DELETE CASCADE,
-raidid integer references Raids(raidid)
+raidid INTEGER REFERENCES Raids(raidid)
 ON DELETE CASCADE,
-reviewid integer references RaidReviews(reviewid)
+reviewid INTEGER REFERENCES RaidReviews(reviewid)
 ON DELETE CASCADE
 );
 
 /* Specific user member of specific party */
 CREATE TABLE Member_of (
-userid integer references users(userid)
+userid INTEGER REFERENCES users(userid)
 ON DELETE CASCADE,
-partyid integer references Parties(partyid)
+partyid INTEGER REFERENCES Parties(partyid)
 ON DELETE CASCADE
 );
 
 /* Specific user owns specific game */
 CREATE TABLE Owns (
-userid integer references users(userid)
+userid INTEGER REFERENCES users(userid)
 ON DELETE CASCADE,
-gameid integer references games(gameid)
+gameid INTEGER REFERENCES games(gameid)
 ON DELETE CASCADE
 );
 
 /* Game that a specific user is interested in along with their skill level */
 CREATE TABLE Interested_in (
-userid integer references users(userid)
+userid INTEGER REFERENCES users(userid)
 ON DELETE CASCADE,
-gameid integer references games(gameid)
+gameid INTEGER REFERENCES games(gameid)
 ON DELETE CASCADE,
-skill integer CONSTRAINT skill_constraint CHECK (skill > 0 AND skill < 6)
+skill INTEGER CONSTRAINT skill_constraint CHECK (skill > 0 AND skill < 6)
 );
 
 /* Specific party hosting specific raid */
 CREATE TABLE Hosting (
-userid integer references parties(partyid)
+userid INTEGER REFERENCES parties(partyid)
 ON DELETE CASCADE,
-raidid integer references raids(raidid)
+raidid INTEGER REFERENCES raids(raidid)
 ON DELETE CASCADE
 );
 
 /* Specific user attending specific raid */
 CREATE TABLE Attending (
-userid integer references users(userid)
+userid INTEGER REFERENCES users(userid)
 ON DELETE CASCADE,
-raidid integer references raids(raidid)
+raidid INTEGER REFERENCES raids(raidid)
 ON DELETE CASCADE
 );
 
 /* Self relationship between different users */
 CREATE TABLE Friends_with (
-userid integer references users(userid)
+userid INTEGER REFERENCES users(userid)
 ON DELETE CASCADE,
-friendid integer references users(userid)
+friendid INTEGER REFERENCES users(userid)
 ON DELETE CASCADE
 );
